b8f101261eceeebee7e71fd59b1df5cba5b7d8f6
AL-MUKSIT <almuksitsazid@gmail.com>
2026-05-22 01:03:07 +0600

    master

--- app/src/main/java/com/smashchat/Models/Messages.java
+++ app/src/main/java/com/smashchat/Models/Messages.java
@@ -7,16 +7,26 @@ public class Messages {
     private String uId;
     private String message;
     private long timestamp;
+    private int type; // 0 for text, 1 for smile emoji
 
     public Messages(String uId, String message, long timestamp) {
         this.uId = uId;
         this.message = message;
         this.timestamp = timestamp;
+        this.type = 0;
     }
 
     public Messages(String uId, String message) {
         this.uId = uId;
         this.message = message;
+        this.type = 0;
+    }
+
+    public Messages(String uId, String message, long timestamp, int type) {
+        this.uId = uId;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.type = type;
     }
 
     public Messages() {
@@ -44,5 +54,13 @@ public class Messages {
 
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 }
